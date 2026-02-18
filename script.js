69a5e610d5b07bf2c28d10c6507c143e46f20307
tstyer <travisstyer.ts@gmail.com>
2026-02-18 13:53:16 +0000

    Add first event listener for 'next'

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Fetches the progress bar */
+const progress = document.getElementById('progress');
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    /* This if statement says that if the currentActive gets above the max
+       amount of circles, then reset it to the max amount with 
+       currentActive = circles.length. Since circles.length is a max of 4 */
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+})
